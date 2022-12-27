bf576cf47f603c1e874b4215a64cba8d5f1f6deb
SUSHANT SUMAN <sushantsuman.sbg@gmail.com>
2022-12-27 16:28:33 +0530

    Update date_conversion.py

--- utilities/date_conversion.py
+++ utilities/date_conversion.py
@@ -2,5 +2,5 @@ from datetime import datetime,timezone
 
 def convert_date():
     date_convert =  datetime.now(timezone.utc).isoformat().replace("+00:00",'')
-    da = str(date_convert).split('.')[0]
-    return da
+    converted_date = str(date_convert).split('.')[0]
+    return converted_date
